35ea441eee87b1b8ce4e97e26f99fc6db478b509
yahyamalik398 <yahyamalik.bscssef20@iba-suk.edu.pk>
2023-10-16 22:22:37 +0500

    init

---
+++ functions.py
@@ -0,0 +1,8 @@
+# def format_name(f_name, l_name):
+#     full_name=(f_name.title()+" "+l_name.title())
+#     return full_name
+# output=format_name("YAhya", "mAliK")
+# print(output)
+output=print(input("what is your name:"))
+
+
